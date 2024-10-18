2fc77e7ed2b6daaabd9106c53cfd3a77bc2aabf0
luka261 <lukamakharadze6@gmail.com>
2024-10-19 01:18:50 +0400

    second commit

---
+++ day00/homework/hw00.py
@@ -0,0 +1,211 @@
+from turtle import *
+
+shape("turtle")
+
+penup()
+goto (-500 , -100)
+pendown()
+
+speed(100000000000)
+
+color ("purple")
+width(5)
+begin_fill()
+forward (200)
+left(90)
+forward (200)
+left(90)
+forward (200)
+left(90)
+forward (200)
+left(90)
+end_fill()
+
+penup()
+goto (-300 , 100)
+pendown()
+
+color ("red")
+begin_fill()
+left (120)
+forward (200)
+left(120)
+forward (200)
+end_fill()
+
+penup()
+goto (-425 , -100)
+pendown()
+
+color("yellow")
+begin_fill()
+left(210)
+forward(100)
+right(90)
+forward(50)
+right(90)
+forward(100)
+right(90)
+forward(50)
+end_fill()
+
+
+penup()
+goto (-440 , 65)
+pendown()
+
+color("blue")
+begin_fill()
+forward (50)
+left(90)
+forward (50)
+left(90)
+forward (50)
+left(90)
+forward (50)
+left(90)
+end_fill()
+
+penup()
+goto (-310 , 65)
+pendown()
+
+color("blue")
+begin_fill()
+forward (50)
+left(90)
+forward (50)
+left(90)
+forward (50)
+left(90)
+forward (50)
+left(90)
+end_fill()
+
+penup()
+goto(200, -100)
+pendown()
+
+
+
+
+
+
+penup()
+goto(200, -100)
+pendown()
+color("brown")
+
+begin_fill()
+forward(200)
+right(90)
+forward(300)
+right(90)
+forward(100)
+right(90)
+forward(200)
+left(90)
+forward(200)
+right(90)
+forward(100)
+right(90)
+forward(100)
+right(90)
+
+end_fill()
+
+
+
+
+
+right(90)
+forward(50)
+color("#a15e35")
+begin_fill()
+left(90)
+forward(70)
+left(90)
+forward(35)
+left(90)
+forward(70)
+left(90)
+forward(35)
+
+end_fill()
+color("#7a4828")
+left(90)
+forward(70)
+left(90)
+forward(35)
+left(90)
+forward(70)
+
+penup()
+goto(200, -100)
+pendown()
+color("#331503")
+
+
+right(90)
+forward(200)
+right(90)
+forward(300)
+right(90)
+forward(100)
+right(90)
+forward(200)
+left(90)
+forward(200)
+right(90)
+forward(100)
+right(90)
+forward(100)
+right(90)
+
+
+penup()
+forward(40)
+pendown()
+
+color("blue")
+begin_fill()
+forward(25)
+left(90)
+forward(170)
+left(90)
+forward(25)
+left(90)
+forward(170)
+end_fill()
+
+penup()
+left(90)
+forward(85)
+left(90)
+forward(135)
+
+pendown()
+begin_fill()
+forward(30)
+right(90)
+forward(150)
+right(90)
+forward(30)
+right(90)
+forward(150)
+right(90)
+
+end_fill()
+
+penup()
+goto(-150, 0)
+pendown()
+color("black")
+right(90)
+
+
+
+
+
+
+exitonclick()
---
+++ day04/homework/hw4.py
@@ -0,0 +1,35 @@
+# 2) დაპრინტეთ Hello world 5'ჯერ (while loop'ით)
+
+x = 0
+
+while x < 10:
+    print('hellow world')
+    x = x + 1
+
+
+
+# 3) დაპრინტეთ Goodbye World 10'ჯერ (for loop'ით)
+
+for e in range (10):
+    print('goodbye world')
+
+
+
+# 4) დაპრინტეთ რიცხვები 0'დან 100'ის ჩათვლით for loop'ის მეშვეობით 
+for e in range (101):
+    print(e)
+
+# 5) დაპრინტეთ რიცხვები 0'დან 100'ის ჩათვლით while loop'ის მეშვეობით 
+while x < 101:
+    print(x)
+    x = x + 1
+# 6) დაპრინტეთ რიცხვები 49'იდან 51'მდე for loop'ის მეშვეობით
+
+for i in range (49, 52):
+    print(i)
+
+# 7) დაპრინტეთ რიცხვები 16'იდან 57'მდე while loop'ის მეშვეობით
+y = 16
+while y < 58:
+    print(y)
+    y = y + 1
