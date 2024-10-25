8a0654ebb6d5adce79960357f7511ace7a8bb2da
luka261 <lukamakharadze6@gmail.com>
2024-10-26 00:27:06 +0400

    second commit

---
+++ day06/classwork/0.0_cw6.py
@@ -0,0 +1,22 @@
+#აქ ვთქვათ ვწერთ  A   B   C  ხოდა აქ რა ხდება
+#A = 0 b =200 c=6  აქ a არის საიდან დაიწყოს  და  b სად დაასრულოს
+# და c მერამდენე რიგშე დაითვალოს აქ არის c = 6
+# ხოდა ყოველ მეექვსე რიცხვს დაპრინტავს 0 დან 200მდე
+# for i in range   (0, 200, 6):
+#     print(i)
+
+# for i in range(1000, 0):
+#     print(i)
+
+
+
+
+# a_student = False
+age = int(input("your age pls")) 
+student = int(input("are you a student?"))
+if age<=18 or student is True :
+
+    print("you got a 50%' discount")
+else:
+    print("nah u get no discount")
+
---
+++ day06/homework/hm6.py
@@ -0,0 +1,60 @@
+
+
+
+
+
+# 2) გამოიყენეთ for loop'ი და გამოიტანეთ 1'დან 10'ის ჩათვლით რიცხვების ნამრავლი
+for y in range (11):
+    print(y)
+    y = y + 1
+# 3) გამოიყენეთ while loop'ი და გამოიტანეთ 1'დან 10'ის ჩათვლით რიცხვების ნამრავლი
+x = 0
+while x <= 10 :
+    print(x)
+    x = x + 1
+# 4) მომხმარებელს შემოატანინეთ რიცხვი და უთხარით ლუწია თუ კენტი. (hints:       10 % 2 = 0;        5 % 2 = 1)
+
+def check_even_odd(number):
+    if number % 2 == 0:
+        return "The number is even."
+    else:
+        return "The number is odd."
+
+
+user_input = input("Please enter a number: ")
+
+
+try:
+    number = int(user_input)
+    result = check_even_odd(number)
+    print(result)
+except ValueError:
+    print("That's not a valid number!")
+
+
+# 5) მომხმარებელს შემოატანინეთ რიცხვი და უთხარით მისი grade ამ ფოტოს მიხედვით: 
+# (ანუ თუ მაგალითად 90'დან 100'ის ჩათვლით ექნება ქულა გამოუტანეთ "Grade A", თუ 80'დან 89'ის ჩათვლით გამოუტანეთ "Grade B" და ა.შ)
+
+
+def check_grade_scores(score):
+    if 90 <= score <= 100:
+        return "You got an A, you nerd!"
+    elif 80 <= score <= 89:
+        return "You got a B, you Asian!"
+    elif 60 <= score <= 79:
+        return "You got a C, you average!"
+    elif 40 <= score <= 59:
+        return "You got a D, you disappointment!"
+    elif 0 <= score <= 39:
+        return "You got an F, you failure!"
+    else:
+        return "Invalid score! Please enter a score between 0 and 100."
+
+user_input = input("Please enter your score: ")
+
+try:
+    score = int(user_input)
+    result = check_grade_scores(score)
+    print(result)
+except ValueError:
+    print("That's not a valid number!")
